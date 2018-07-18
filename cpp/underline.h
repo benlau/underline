3e92bb443bf3fd0864b35e8426a020a0d4eb00a2
Ben Lau <xbenlau@gmail.com>
2018-07-19 00:10:53 +0800

    Import assign/set/get/pick/omit functions from QSyncable project

--- cpp/underline.h
+++ cpp/underline.h
@@ -2,6 +2,8 @@
 #include <QList>
 #include <QStringList>
 #include <QVariantList>
+#include <QVariantMap>
+#include <QObject>
 #include <functional>
 
 namespace _ {
@@ -164,4 +166,271 @@ namespace _ {
 
         return res;
     }
+
+    /// Gets the value at path of object. If the path is not found, the defaultValue is returned.
+    /*
+     Example:
+
+     get(object, "a.b.c");
+
+    */
+
+
+    /// Sets the value at path of object. If a portion of path doesn't exist, it's created.
+    /*
+     Example:
+
+     set(data, "a.b", 3); // data["a"] will be a QVariantMap that contains a key of "b".
+
+     */
+
+
+
+    /*
+     If a property contains QObject pointer, it will be converted to QVariantMap.
+
+     In case you need to obtain a QObject pointer, please use get().
+     */
+
+    /// Assign properties from source object to the destination object.
+    inline void assign(QVariantMap &dest, const QObject *source)
+    {
+        const QMetaObject* meta = source->metaObject();
+
+        for (int i = 0 ; i < meta->propertyCount(); i++) {
+            const QMetaProperty property = meta->property(i);
+            QString p = property.name();
+
+            QVariant value = source->property(property.name());
+
+            if (value.canConvert<QObject*>()) {
+                QVariantMap map;
+                assign(map, value.value<QObject*>()); // nested properties is not supported yet
+                value = map;
+            }
+
+            dest[p] = value;
+        }
+
+    }
+
+    inline void assign(QObject *dest, const QVariantMap & source)
+    {
+        const QMetaObject* meta = dest->metaObject();
+
+        QMap<QString,QVariant>::const_iterator iter = source.begin();
+        while (iter != source.end()) {
+            QByteArray key = iter.key().toLocal8Bit();
+
+            int index = meta->indexOfProperty(key.constData());
+            if (index < 0) {
+                qWarning() << QString("assign:assign a non-existed property: %1").arg(iter.key());
+                iter++;
+                continue;
+            }
+
+            QVariant orig = dest->property(key.constData());
+            QVariant value = source[iter.key()];
+
+            if (orig.canConvert<QObject*>()) {
+                if (value.type() != QVariant::Map) {
+                    qWarning() << QString("assign:expect a QVariantMap property but it is not: %1");
+                } else {
+                    assign(orig.value<QObject*>(), value.toMap());
+                }
+
+            } else if (orig != value) {
+                dest->setProperty(key.constData(), value);
+            }
+
+            iter++;
+        }
+    }
+
+    inline QVariant _get(const QVariantMap& object, const QStringList &path, const QVariant& defaultValue) ;
+
+    inline QVariant _get(const QObject* object, const QStringList &path, const QVariant& defaultValue) {
+
+        QString key = path[0];
+
+        const QMetaObject* meta = object->metaObject();
+
+        if (meta->indexOfProperty(key.toUtf8().constData()) < 0) {
+            return defaultValue;
+        }
+
+        QVariant value = object->property(key.toUtf8().constData());
+
+        if (path.size() == 1) {
+            return value;
+        }
+
+        QStringList nextPath = path;
+        nextPath.removeFirst();
+
+        if (value.canConvert<QObject*>()) {
+            return _get(qvariant_cast<QObject*>(value), nextPath, defaultValue);
+        } else if (value.type() == QVariant::Map) {
+            return _get(value.toMap(), nextPath, defaultValue);
+        } else {
+            return defaultValue;
+        }
+    }
+
+    inline QVariant _get(const QVariantMap& object, const QStringList &path, const QVariant& defaultValue) {
+
+        QString key = path[0];
+
+        if (!object.contains(key)) {
+            return defaultValue;
+        }
+
+        QVariant value = object[key];
+
+        if (path.size() == 1) {
+            return value;
+        }
+
+        QStringList nextPath = path;
+        nextPath.removeFirst();
+
+        if (value.canConvert<QObject*>()) {
+            return _get(qvariant_cast<QObject*>(value), nextPath, defaultValue);
+        } else if (value.type() == QVariant::Map) {
+            return _get(value.toMap(), nextPath, defaultValue);
+        } else {
+            return defaultValue;
+        }
+    }
+
+    inline QVariant get(const QObject *object, const QStringList &path, const QVariant& defaultValue)
+    {
+        return _get(object, path, defaultValue);
+    }
+
+    inline QVariant get(const QObject *object, const QString &path, const QVariant& defaultValue = QVariant())
+    {
+        return get(object, path.split("."), defaultValue);
+    }
+
+    inline QVariant get(const QVariantMap &source, const QStringList &path, const QVariant &defaultValue = QVariant())
+    {
+        return _get(source, path, defaultValue);
+    }
+
+    inline QVariant get(const QVariantMap &source, const QString &path, const QVariant &defaultValue = QVariant())
+    {
+        return get(source, path.split("."), defaultValue);
+    }
+
+    inline void set(QVariantMap &data, const QStringList &path, const QVariant &value)
+    {
+        QString key = path[0];
+
+        if (path.size() == 1) {
+            data[key] = value;
+        } else {
+            if (!data.contains(key) || !data[key].canConvert<QVariantMap>()) {
+                data[key] = QVariantMap();
+            }
+            QStringList nextPath = path;
+            nextPath.removeFirst();
+            QVariantMap map = data[key].toMap();
+            set(map, nextPath, value);
+            data[key] = map;
+        }
+    }
+
+    inline void set(QVariantMap &data, const QString &path, const QVariant &value)
+    {
+        return set(data, path.split("."), value);
+    }
+    /// Creates an QVariantMap composed of the picked object properties at paths.
+    /*
+     Example:
+
+         pick(object, QStringList() << "a" << "b.c");
+
+     If a property contains QObject pointer, it will be converted to QVariantMap.
+
+     In case you need to obtain a QObject pointer, please use get().
+
+     */
+
+    inline QVariantMap pick(QObject *object, const QStringList &paths)
+    {
+        QVariantMap data;
+        foreach (QString path, paths) {
+            QVariant value = get(object, path);
+            if (value.isNull()) {
+                continue;
+            }
+
+            if (value.canConvert<QObject*>()) {
+                QVariantMap map;
+                assign(map, value.value<QObject*>());
+                value = map;
+            }
+
+            set(data, path, value);
+        }
+        return data;
+    }
+
+
+    inline QVariantMap pick(QVariantMap source, const QStringList &paths)
+    {
+        QVariantMap data;
+        foreach (QString path, paths) {
+            QVariant value = get(source, path);
+            if (value.isNull()) {
+                continue;
+            }
+
+            if (value.canConvert<QObject*>()) {
+                QVariantMap map;
+                assign(map, value.value<QObject*>());
+                value = map;
+            }
+
+            set(data, path, value);
+        }
+        return data;
+    }
+
+    inline QVariantMap pick(QVariantMap source, const QVariantMap &paths)
+    {
+        return pick(source, paths.keys());
+    }
+
+    /// The opposite of pick(), this method creates an QVariantMap composed of the own properties that are not omitted.
+
+    inline QVariantMap omit(const QVariantMap &source, const QVariantMap &properties)
+    {
+
+        QMap<QString,QVariant>::const_iterator iter = source.begin();
+        QVariantMap result;
+
+        while (iter != source.end()) {
+
+            if (properties.contains(iter.key())) {
+                iter++;
+                continue;
+            }
+
+            QVariant value = source[iter.key()];
+
+            if (value.canConvert<QObject*>()) {
+                QVariantMap map;
+                assign(map, value.value<QObject*>());
+                value = map;
+            }
+
+            result[iter.key()] = value;
+            iter++;
+        }
+
+        return result;
+    }
+
 }
