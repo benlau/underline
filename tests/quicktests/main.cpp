a8d68066450a0f9a23d0fcf21ab1f4aec2b9f72f
Ben Lau <xbenlau@gmail.com>
2018-09-09 10:57:45 +0800

    Fix QuickTests broken build issue

--- tests/quicktests/main.cpp
+++ tests/quicktests/main.cpp
@@ -1,27 +1,15 @@
 #include <QString>
 #include <QtTest>
-#include <TestRunner>
-#include <QtQuickTest/quicktest.h>
-#include <XBacktrace.h>
-#include <QtShell>
 #include "quicktests.h"
 #include "underline.h"
 
 int main(int argc, char *argv[])
 {
-    XBacktrace::enableBacktraceLogOnUnhandledException();
     qputenv("QML_DISABLE_DISK_CACHE", "true");
 
-    QCoreApplication app(argc, argv);
-
-    TestRunner runner;
-    runner.add<QuickTests>();
-
-    bool error = runner.exec(app.arguments());
-
-    if (!error) {
-        qDebug() << "All test cases passed!";
-    }
-
-    return error;
+    QApplication app(argc, argv);
+    app.setAttribute(Qt::AA_Use96Dpi, true);
+    QuickTests tc;
+    return QTest::qExec(&tc, argc, argv);
 }
+
